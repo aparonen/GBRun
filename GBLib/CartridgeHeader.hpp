20f978765a607b6436b0997ed32e893abfc81d09
Anton Paronen <anton.lauri.paronen@gmail.com>
2021-05-16 14:28:10 +0300

    Create a library

---
+++ GBLib/CartridgeHeader.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <array>
+#include <cstdint>
+
+namespace GBLib {
+
+struct CartridgeHeader {
+    std::array<uint8_t, 16> title;
+    std::array<uint8_t, 2> newLicenseeCode;
+    uint8_t sgbFlag;
+    uint8_t cartridgeType;
+    uint8_t romSize;
+    uint8_t ramSize;
+    uint8_t destinationCode;
+    uint8_t oldLicenseeCode;
+    uint8_t maskROMVersionNumber;
+    uint8_t headerChecksum;
+    uint16_t globalChecksum;
+};
+
+} // namespace GBLib
