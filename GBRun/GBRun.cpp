160fa7f93f1af8f0099190053309720942f9fed3
Anton Paronen <anton.lauri.paronen@gmail.com>
2021-05-24 11:59:02 +0300

    Add test code

--- GBRun/GBRun.cpp
+++ GBRun/GBRun.cpp
@@ -1,10 +1,28 @@
 ﻿#include "GBRun.h"
 
+#include <cstdint>
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <vector>
 
-int main()
+#include <CartridgeROM.hpp>
+
+int main(int argc, char** argv)
 {
-    std::cout << "Hello, World!\n";
+    if (argc != 2) {
+        std::cout << "Provide a cartridge ROM dump as an argument\n";
+        return EXIT_FAILURE;
+    }
+
+    std::ifstream ifs(argv[1], std::ios::binary | std::ios::ate);
+    const auto size = ifs.tellg();
+    ifs.seekg(0);
+
+    std::vector<uint8_t> bytes(size);
+    ifs.read(reinterpret_cast<char*>(bytes.data()), size);
+
+    const GBLib::CartridgeROM rom(bytes);
+
     return EXIT_SUCCESS;
 }
